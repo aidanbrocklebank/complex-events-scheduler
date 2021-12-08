095cd7b0c5613ab314b9a96acd17adac5bb8154b
Aidan Brocklebank <60949374+aidanbrocklebank@users.noreply.github.com>
2021-12-08 23:28:15 +0000

    Add coordinator class for second algorithm.

---
+++ src/uk/ac/cam/agb67/dissertation/algorithm/two/Coordinator.java
@@ -0,0 +1,15 @@
+package uk.ac.cam.agb67.dissertation.algorithm.two;
+
+import uk.ac.cam.agb67.dissertation.*;
+
+public class Coordinator implements SchedulingAlgorithm {
+
+    public Coordinator() {
+
+    }
+
+    @Override
+    public Timetable generate(SchedulingProblem details) {
+        return null;
+    }
+}
