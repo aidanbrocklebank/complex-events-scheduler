13b3e873dde2f176fa827b25efe12fb6ae5e0915
Aidan Brocklebank <60949374+aidanbrocklebank@users.noreply.github.com>
2021-12-21 18:52:22 +0000

    Implement tests for the random number methods.

--- test/uk/ac/cam/agb67/dissertation/MainTest.java
+++ test/uk/ac/cam/agb67/dissertation/MainTest.java
@@ -184,6 +184,53 @@ public class MainTest {
         }
     }
 
-    // TODO implement real tests for the randomised data
+    @Test
+    public void generate_number_returns_int_within_range() {
+        // ARRANGE
+        int max = 105; int min = 7;
+
+        for (int i=0; i<100; i++) {
+            // ACT
+
+            int random = Main.generate_number(max, min);
+
+            // ASSERT
+            assertThat(random).isAtMost(max);
+            assertThat(random).isAtLeast(min);
+        }
+    }
+
+    @Test
+    public void generate_numbers_returns_ints_within_range() {
+        // ARRANGE
+        int max = 1205; int min = 78;
+
+        // ACT
+        List<Integer> random = Main.generate_numbers(max, min, 100);
+
+        // ASSERT
+        for (int i=0; i<100; i++) {
+            assertThat(random.get(i)).isAtMost(max);
+            assertThat(random.get(i)).isAtLeast(min);
+        }
+    }
+
+    @Test
+    public void generate_numbers_returns_no_duplicates() {
+        // ARRANGE
+        int max = 1205; int min = 78;
+        int[] occurrences = new int[max-min];
+
+        // ACT
+        List<Integer> random = Main.generate_numbers(max, min, 100);
+
+        // ASSERT
+        for (int i=0; i<100; i++) {
+            occurrences[random.get(i) - min] += 1;
+        }
+        for (int i=0; i<max-min; i++) {
+            assertThat(occurrences[i]).isAtMost(1);
+        }
+    }
 
 }
