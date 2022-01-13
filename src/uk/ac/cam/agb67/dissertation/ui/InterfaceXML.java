ba3bded9c620eb20ecec1966e12051a12301118a
Aidan Brocklebank <60949374+aidanbrocklebank@users.noreply.github.com>
2022-01-13 17:05:32 +0000

    Implement tests and fixes for the XML decoder.

--- src/uk/ac/cam/agb67/dissertation/ui/InterfaceXML.java
+++ src/uk/ac/cam/agb67/dissertation/ui/InterfaceXML.java
@@ -16,97 +16,103 @@ public class InterfaceXML {
 
     // Takes the location of an XML file representing a user input and returns the decoded SchedulingProblem
     public SchedulingProblem XML_to_Problem(String filename) {
+        try {
 
-        // Create the scheduling problem to populate
-        SchedulingProblem details = new SchedulingProblem();
-        details.Session_Details = new ArrayList<>();
-        details.KeyInd_Details = new ArrayList<>();
-        details.PDS_Details = new ArrayList<>();
-        int sid = 0, pdsid = 0;
+            // Create the scheduling problem to populate
+            SchedulingProblem details = new SchedulingProblem();
+            details.Session_Details = new ArrayList<>();
+            details.KeyInd_Details = new ArrayList<>();
+            details.PDS_Details = new ArrayList<>();
+            int sid = 0, pdsid = 0;
 
-        // Get every object from the XML diagram, each of which represents either a session or an individual
-        ReadXmlDomParser readXMLDomParser = new ReadXmlDomParser(filename);
-        NodeList list = readXMLDomParser.getElementsByTagName("object");
+            // Get every object from the XML diagram, each of which represents either a session or an individual
+            ReadXmlDomParser readXMLDomParser = new ReadXmlDomParser(filename);
+            NodeList list = readXMLDomParser.getElementsByTagName("object");
 
-        // Iterate through and turn them into the necessary lists
-        for (int i=0; i<list.getLength(); i++) {
-            Node object_node = list.item(i);
-            if (object_node.getNodeType() == Node.ELEMENT_NODE) {
-                Element object = (Element) object_node;
+            // Iterate through and turn them into the necessary lists
+            for (int i = 0; i < list.getLength(); i++) {
+                Node object_node = list.item(i);
+                if (object_node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element object = (Element) object_node;
 
-                // Determine if the object is a session, a key individual, or the overall details
-                NodeList innerCell = object.getElementsByTagName("mxCell");
-                String object_style_info = innerCell.item(0).getAttributes().getNamedItem("style").getTextContent();
-                //if (Main.DEBUG) System.out.println("Style info for object "+i+" was: "+object_style_info);
+                    // Determine if the object is a session, a key individual, or the overall details
+                    NodeList innerCell = object.getElementsByTagName("mxCell");
+                    String object_style_info = innerCell.item(0).getAttributes().getNamedItem("style").getTextContent();
+                    //if (Main.DEBUG) System.out.println("Style info for object "+i+" was: "+object_style_info);
 
-                //NOTE: Event's will have rounded=0, while key individuals will have rounded=1
-                if (object_style_info.substring(0, 9).equals("rounded=1")) {
-                    // Decode a key individual
-                    if (Main.DEBUG) System.out.println("Found one key individual.");
+                    //NOTE: Event's will have rounded=0, while key individuals will have rounded=1
+                    if (object_style_info.substring(0, 9).equals("rounded=1")) {
+                        // Decode a key individual
+                        if (Main.DEBUG) System.out.println("Found one key individual.");
 
-                    String individual_name = object.getAttribute("label");
-                    String individual_DLP = object.getAttribute("DailyLimitPreference");
-                    String individual_room_prefs = object.getAttribute("RoomPreferences");
+                        String individual_name = object.getAttribute("label");
+                        String individual_DLP = object.getAttribute("DailyLimitPreference");
+                        String individual_room_prefs = object.getAttribute("RoomPreferences");
 
-                    KeyIndividual keyInd = new KeyIndividual(individual_name, Integer.parseInt(individual_DLP), string_to_list(individual_room_prefs));
-                    details.KeyInd_Details.add(keyInd);
+                        KeyIndividual keyInd = new KeyIndividual(individual_name, Integer.parseInt(individual_DLP), string_to_list(individual_room_prefs));
+                        details.KeyInd_Details.add(keyInd);
 
-                } else if (object_style_info.equals("rounded=0;whiteSpace=wrap;html=1;strokeWidth=2;")) {
-                    // Decode a predetermined event
-                    if (Main.DEBUG) System.out.println("Found one PDS event.");
+                    } else if (object_style_info.equals("rounded=0;whiteSpace=wrap;html=1;strokeWidth=2;")) {
+                        // Decode a predetermined event
+                        if (Main.DEBUG) System.out.println("Found one PDS event.");
 
-                    String session_name = object.getAttribute("label");
-                    String session_length = object.getAttribute("length");
-                    String session_participants = object.getAttribute("participants");
+                        String session_name = object.getAttribute("label");
+                        String session_length = object.getAttribute("length");
+                        String session_participants = object.getAttribute("participants");
 
-                    String PDS_day = object.getAttribute("day");
-                    String PDS_time = object.getAttribute("hour");
-                    String PDS_room = object.getAttribute("room");
+                        String PDS_day = object.getAttribute("day");
+                        String PDS_time = object.getAttribute("hour");
+                        String PDS_room = object.getAttribute("room");
 
-                    PredeterminedSession pds = new PredeterminedSession(pdsid, session_name, Integer.parseInt(session_length), string_to_list(session_participants),
-                            Integer.parseInt(PDS_day), Integer.parseInt(PDS_time), Integer.parseInt(PDS_room));
-                    details.PDS_Details.add(pds);
-                    pdsid++;
+                        PredeterminedSession pds = new PredeterminedSession(pdsid, session_name, Integer.parseInt(session_length), string_to_list(session_participants),
+                                Integer.parseInt(PDS_day), Integer.parseInt(PDS_time), Integer.parseInt(PDS_room));
+                        details.PDS_Details.add(pds);
+                        pdsid++;
 
-                } else if (object_style_info.substring(0, 9).equals("rounded=0")) {
-                    // Decode a normal event
-                    if (Main.DEBUG) System.out.println("Found one event.");
+                    } else if (object_style_info.substring(0, 9).equals("rounded=0")) {
+                        // Decode a normal event
+                        if (Main.DEBUG) System.out.println("Found one event.");
 
-                    String session_name = object.getAttribute("label");
-                    String session_length = object.getAttribute("length");
-                    String session_participants = object.getAttribute("participants");
+                        String session_name = object.getAttribute("label");
+                        String session_length = object.getAttribute("length");
+                        String session_participants = object.getAttribute("participants");
 
-                    Session sesh = new Session(sid, session_name, Integer.parseInt(session_length), string_to_list(session_participants));
-                    details.Session_Details.add(sesh);
-                    sid++;
+                        Session sesh = new Session(sid, session_name, Integer.parseInt(session_length), string_to_list(session_participants));
+                        details.Session_Details.add(sesh);
+                        sid++;
 
-                } else if (object_style_info.substring(0,14).equals("shape=process;")) {
-                    // Decode the overall event details
-                    if (Main.DEBUG) System.out.println("Found the overall event details.");
+                    } else if (object_style_info.substring(0, 14).equals("shape=process;")) {
+                        // Decode the overall event details
+                        if (Main.DEBUG) System.out.println("Found the overall event details.");
 
-                    details.Maximum_Days = Integer.parseInt(object.getAttribute("Days"));
-                    details.Hours_Per_Day = Integer.parseInt(object.getAttribute("HoursPerDay"));
-                    details.Maximum_Rooms = Integer.parseInt(object.getAttribute("Rooms"));
+                        details.Maximum_Days = Integer.parseInt(object.getAttribute("Days"));
+                        details.Hours_Per_Day = Integer.parseInt(object.getAttribute("HoursPerDay"));
+                        details.Maximum_Rooms = Integer.parseInt(object.getAttribute("Rooms"));
 
-                    details.Room_Occupancy_Limits = string_to_list(object.getAttribute("RoomCapacities"));
+                        details.Room_Occupancy_Limits = string_to_list(object.getAttribute("RoomCapacities"));
 
-                    details.Reduce_Overlap_Pref = Boolean.parseBoolean(object.getAttribute("ReduceOverlapPreference"));
-                    details.Minimum_Gap_Pref = Integer.parseInt(object.getAttribute("BookingGapPreference"));
+                        details.Reduce_Overlap_Pref = Boolean.parseBoolean(object.getAttribute("ReduceOverlapPreference"));
+                        details.Minimum_Gap_Pref = Integer.parseInt(object.getAttribute("BookingGapPreference"));
 
-                } else {
-                    System.err.println("Discovered an object in the XML file which does not correspond to input data.");
+                    } else {
+                        System.err.println("Discovered an object in the XML file which does not correspond to input data.");
+                    }
                 }
             }
-        }
 
-        // Then add the predetermined sessions to the sessions list
-        for (int i=0; i<pdsid; i++) {
-            // Update all PDS to have IDs at the end of the list
-            details.PDS_Details.get(i).Session_ID += sid;
-            details.Session_Details.add(details.PDS_Details.get(i));
-        }
+            // Then add the predetermined sessions to the sessions list
+            for (int i = 0; i < pdsid; i++) {
+                // Update all PDS to have IDs at the end of the list
+                details.PDS_Details.get(i).Session_ID += sid;
+                details.Session_Details.add(details.PDS_Details.get(i));
+            }
 
-        return details;
+            return details;
+
+        } catch (NumberFormatException e) {
+            System.err.println("There were incorrectly formated values in the XML input.");
+            return null;
+        }
     }
 
     // Takes a finished Timetable and returns the location a an XML file which now contains a visual representation of the schedule
@@ -117,7 +123,7 @@ public class InterfaceXML {
     }
 
     // Takes a string of digits separated by commas and returns a list of numbers
-    private List<Integer> string_to_list(String numbers) {
+    List<Integer> string_to_list(String numbers) throws NumberFormatException {
         List<Integer> list = new ArrayList<>();
 
         int length = numbers.length();
@@ -131,8 +137,13 @@ public class InterfaceXML {
 
             // Decode the number before the comma
             //System.out.println("substring is : "+numbers.substring(0,loc));
-            int num = Integer.parseInt(numbers.substring(0,loc));
-            list.add(num);
+            try {
+                int num = Integer.parseInt(numbers.substring(0, loc));
+                list.add(num);
+            } catch (NumberFormatException e ) {
+                System.err.println("Tried to read in a list which contained non-integer items. \n   Offending Item: '"+numbers.substring(0, loc)+"'");
+                throw e;
+            }
 
             // Remove the number and its comma
             if (loc+1 > length) break;
--- test/uk/ac/cam/agb67/dissertation/ui/InterfaceXMLTest.java
+++ test/uk/ac/cam/agb67/dissertation/ui/InterfaceXMLTest.java
@@ -7,13 +7,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import uk.ac.cam.agb67.dissertation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RunWith(JUnit4.class)
 public class InterfaceXMLTest {
 
     // TODO create some proper tests
 
     @Test
-    public void object_can_be_created(){
+    public void object_can_be_created() {
         // ARRANGE
         InterfaceXML ui;
         // ACT
@@ -24,25 +27,78 @@ public class InterfaceXMLTest {
     }
 
     @Test
-    public void temp() {
+    public void string_to_list_works() {
+        // ARRANGE
+        String nums = "2,5,6,3,3";
+        List<Integer> true_list = Arrays.asList(2, 5, 6, 3, 3);
+        InterfaceXML ui = new InterfaceXML();
+
+        // ACT
+        List<Integer> num_list = ui.string_to_list(nums);
+
+        // ASSERT
+        assertThat(num_list.hashCode()).isEqualTo(true_list.hashCode());
+    }
+
+    @Test
+    public void string_to_list_fails_gracefully() {
+        // ARRANGE
+        String nums = "two, five, six, three, three";
+        List<Integer> true_list = Arrays.asList(2, 5, 6, 3, 3);
+        InterfaceXML ui = new InterfaceXML();
+
+        // ACT
+        try {
+            List<Integer> num_list = ui.string_to_list(nums);
+        } catch (Exception e) {
+            // ASSERT
+            assertThat(e.getClass()).isEqualTo(NumberFormatException.class);
+        }
+    }
+
+    @Test
+    public void XML_decoder_works() {
         // ARRANGE
         InterfaceXML ui;
+
         // ACT
         ui = new InterfaceXML();
         SchedulingProblem details = ui.XML_to_Problem("samples\\FullInput.xml");
 
-        System.out.println(details.Session_Details.toString());
-        System.out.println(details.KeyInd_Details.toString());
+        if (Main.DEBUG) {
+            System.out.println(details.Session_Details.toString());
+            System.out.println(details.KeyInd_Details.toString());
+            System.out.println("\n" + details.Maximum_Days);
+            System.out.println(details.Hours_Per_Day);
+            System.out.println(details.Maximum_Rooms);
+            System.out.println("\n" + details.Room_Occupancy_Limits.toString());
+            System.out.println("\n" + details.Reduce_Overlap_Pref);
+            System.out.println(details.Minimum_Gap_Pref);
+        }
 
-        System.out.println("\n" + details.Maximum_Days);
-        System.out.println(details.Hours_Per_Day);
-        System.out.println(details.Maximum_Rooms);
+        // ASSERT
+        assertThat(details.Session_Details.size()).isEqualTo(4);
+        assertThat(details.KeyInd_Details.size()).isEqualTo(2);
+        assertThat(details.PDS_Details.size()).isEqualTo(1);
 
-        System.out.println("\n" + details.Room_Occupancy_Limits.toString());
+        assertThat(details.Session_Details.get(0).Session_Name).isEqualTo("Group Learning");
+        assertThat(details.Session_Details.get(0).Session_KeyInds.toString()).isEqualTo("[0, 6, 15]");
+        assertThat(details.KeyInd_Details.get(1).KeyInd_Name).isEqualTo("Jim Baxter");
+        assertThat(details.KeyInd_Details.get(1).KeyInd_Daily_Limit_Pref).isEqualTo(2);
+        assertThat(details.Session_Details.get(3).getClass()).isEqualTo(PredeterminedSession.class);
+    }
 
-        System.out.println("\n" + details.Reduce_Overlap_Pref);
-        System.out.println(details.Minimum_Gap_Pref);
+    @Test
+    public void XML_decoder_fails_gracefully() {
+        // ARRANGE
+        InterfaceXML ui;
 
+        // ACT
+        ui = new InterfaceXML();
+        SchedulingProblem details = ui.XML_to_Problem("samples\\IncorrectInput.xml");
+
+        // ASSERT
+        assertThat(details).isEqualTo(null);
     }
 
 }
